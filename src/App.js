9a68199c3618022e90f17c1b5b11e900c74a2766
Chris Grant <chrisgrant303@gmail.com>
2020-08-19 15:34:05 -0500

    Initializing Routers

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 function App() {
-  return (
-    //BEM naming convention
-    <div className="app">
-      <h1>Hello Chris, let's build a slack clone 🚀 </h1>
-
-      <Header />
-      <div className="app_body">
-        <Sidebar />
-        {/* React Router -> Chat screen */}
-      </div>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Header/>
+            <div className="app_body">
+                <Sidebar/> {/* React Router -> Chat screen */} </div>
+        </div>
+    );
 }
 
 export default App;
