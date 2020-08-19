7d7edc85087cdef814fa57d4a0735bfaf5d4f6cc
Chris Grant <chrisgrant303@gmail.com>
2020-08-19 07:50:42 -0500

    Firebase initialization

---
+++ src/Firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCFFtvA9K34D5wbwJtuZIXiJllKayspWJg",
+    authDomain: "slack-clone-22a14.firebaseapp.com",
+    databaseURL: "https://slack-clone-22a14.firebaseio.com",
+    projectId: "slack-clone-22a14",
+    storageBucket: "slack-clone-22a14.appspot.com",
+    messagingSenderId: "1087761807357",
+    appId: "1:1087761807357:web:9668e1e4d068b3788c8b94"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
