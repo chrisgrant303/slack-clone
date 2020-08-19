fa7bd5e744661fc2ad6fecf35e0ee8d7e6c68151
Chris Grant <chrisgrant303@gmail.com>
2020-08-19 07:16:57 -0500

    Sidebar Initialization 80% Complete

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,42 @@
 import React from 'react'
 import './Sidebar.css';
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import CreateIcon from "@material-ui/icons/Create";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import SidebarOption from './SidebarOption';
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+
 
 function Sidebar() {
-    return <div className="sidebar">
-        <div className="sidebar_header">
+    return (
+        <div className="sidebar">
+            <div className="sidebar_header">
+                <div className="sidebar_info">
+                    <h2>Vantage Peak</h2>
+                    <h3>
+                        <FiberManualRecordIcon />
+                    Chris Grant
+                </h3>
+                </div>
+                <CreateIcon />
+            </div>
+            <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+            <SidebarOption Icon={InboxIcon} title="Mentions & Reactions" />
+            <SidebarOption Icon={DraftsIcon} title="Saved items" />
+            <SidebarOption Icon={BookmarkBorderIcon} title="Channel Browser" />
+            <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+            <SidebarOption Icon={AppsIcon} title="Apps" />
+            <SidebarOption Icon={FileCopyIcon} title="File browser" />
+            <SidebarOption Icon={ExpandLessIcon} title="Show less" />
 
         </div>
-    </div>
+    );
 }
 
 export default Sidebar
