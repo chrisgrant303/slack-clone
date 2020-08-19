99705f36b554377a8b30c346060aa78b126d17a5
Chris Grant <chrisgrant303@gmail.com>
2020-08-18 22:23:16 -0500

    Header Initialization complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>Hello Chris, let's build a slack clone 🚀 </h1>
 
       {/* Header */}
+      <Header />
       {/* Sidebar */}
       {/* React Router -> Chat screen */}
 
--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './Header.css';
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime"
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+
 
 function Header() {
-    return <div className="header">
-        <div className='header_left'>
-            {/* Avatars for logged in user */}
-            {/* Time icon */}
+    return (
+        <div className="header">
+            <div className='header_left'>
+                <Avatar className="header_avatar" alt='Chris Grant' src='' />
+                <AccessTimeIcon />
+            </div>
+            <div className="header_search">
+                <SearchIcon />
+                <input placeholder="Search Chris Grant"></input>
+            </div>
+            <div className="header_right">
+                <HelpOutlineIcon />
+            </div>
         </div>
-        <div className="header_search">
-            {/* Search icon */}
-            {/* input */}
-        </div>
-        <div className="header_right">
-            {/* help icon */}
-        </div>
-    </div>
+    )
 }
 
 export default Header
